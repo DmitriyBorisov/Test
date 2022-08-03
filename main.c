a1804ddbe89e0a2d108957fe0e5bbc17d385514d
Dmitriy Borisov <dmitriy.borisov@a-mobile.biz>
2022-08-03 17:11:16 +0300

    Add command line Name argument

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Hello world!\n");
+	if (argc == 2) 
+	{
+		printf("Hello %s!", argv[2]);
+	}
+	else
+	{
+		printf("Hello world!\n");
+	}
 	return 0;
 }
